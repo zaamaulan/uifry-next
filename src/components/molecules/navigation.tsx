7fe4ef5ba45a01458ae0be0a5762b7badac131fd
zaamaulan <zaamaulan@gmail.com>
2025-03-25 20:13:27 +0700

    create sidebar mobile

--- src/components/molecules/navigation.tsx
+++ src/components/molecules/navigation.tsx
@@ -7,6 +7,21 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { Button } from "../ui/button";
 
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+
+type NavLink = {
+  name: string;
+  href: string;
+};
+
 const navLinks = [
   { name: "Home", href: "/" },
   { name: "About Us", href: "/#" },
@@ -15,10 +30,8 @@ const navLinks = [
 ];
 
 export const Navigation = () => {
-  const pathname = usePathname();
-
   return (
-    <nav className="container mx-auto mt-10">
+    <nav className="container relative mx-auto max-md:p-4 md:mt-10">
       <div className="flex items-center justify-between max-md:hidden">
         <div className="flex items-center gap-x-10">
           <Link href="/">
@@ -32,30 +45,71 @@ export const Navigation = () => {
           </Link>
 
           <ul className="flex space-x-4">
-            {navLinks.map((link, index) => {
-              const active =
-                pathname === link.href
-                  ? "text-carnation-400 font-bold"
-                  : "font-medium";
-
-              return (
-                <li key={index}>
-                  <Link
-                    href={link.href}
-                    className={cn("text-xl font-bold text-black", active)}
-                  >
-                    {link.name}
-                  </Link>
-                </li>
-              );
-            })}
+            <Navlink links={navLinks} />
           </ul>
         </div>
 
         <Button>Download</Button>
       </div>
 
-      <div></div>
+      <div className="flex items-center justify-between md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <Menu />
+            <span className="sr-only">Menu</span>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle className="hidden">
+                Are you absolutely sure?
+              </SheetTitle>
+              <SheetDescription className="hidden">
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+
+            <div className="flex flex-col gap-6">
+              <Image
+                src="/logo-black.svg"
+                alt="logo"
+                width={117}
+                height={34.41}
+                priority
+              />
+              <ul className="flex flex-col gap-4">
+                <Navlink links={navLinks} />
+              </ul>
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
     </nav>
+  );
+};
+
+const Navlink = ({ links }: { links: NavLink[] }) => {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {links.map((link, index) => {
+        const active =
+          pathname === link.href
+            ? "text-carnation-400 font-bold"
+            : "font-medium";
+
+        return (
+          <li key={index}>
+            <Link
+              href={link.href}
+              className={cn("text-xl font-bold text-black", active)}
+            >
+              {link.name}
+            </Link>
+          </li>
+        );
+      })}
+    </>
   );
 };
